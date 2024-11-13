02a274c828cad10d54ef3816e2f295bff63cde1d
francescoventitre <francesco.orru23@gmail.com>
2024-11-13 14:28:01 +0100

    EsercizioS2L3

---
+++ EsercizioS2L3.py
@@ -0,0 +1,4 @@
+#Programma per generare il nome di una band
+citta=input("Inserisci la tua città di origine: ") #Chiede all'utente di inserire il nome della sua città di origine
+animale=input("Inserisci il nome del tuo animale domestico: ") #Chiede all'utente di inserire il nome del proprio animale domestico
+print (f"Il nome generato per la tua band è: {citta + animale}") #Stampa il risultato sommando nome della città e nome dell'animale
